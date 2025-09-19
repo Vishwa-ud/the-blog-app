2ca1e92f9c982a4042b26fbab7e15daad303b627
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-09-20 01:03:43 +0530

    sql injection prevention mechanism added to the comment

--- backend/src/routes/comments.ts
+++ backend/src/routes/comments.ts
@@ -1,9 +1,14 @@
-import { createComment, getPostComments } from "../controllers/comments";
+import { 
+    createComment, 
+    getPostComments,
+    createCommentValidation,
+    getPostCommentsValidation
+} from "../controllers/comments";
 import { authMiddleware } from "../middleware/authMiddleware";
 import express from "express";
 
 const router = express.Router();
-router.get("/post/:id", authMiddleware, getPostComments);
-router.post("/", authMiddleware, createComment);
+router.get("/post/:id", authMiddleware, getPostCommentsValidation, getPostComments);
+router.post("/", authMiddleware, createCommentValidation, createComment);
 
 export default router;
